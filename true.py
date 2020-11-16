6260a1c26ffb91237a721f6d307bdb8e7283d0bb
Felex <felokemboi10@gmail.com>
2020-11-16 21:46:11 +0300

    Delete true.py

--- true.py
+++
@@ -1,6 +0,0 @@
-originalsample = [[1,2,3],[3,4,5],[1,6,7],[2,4,6],[2,5,7],[3,4,7],[3,5,6]]
-sample = [[1,2,3],[3,4,5],[1,6,7],[2,4,6],[2,5,7],[3,4,7],[3,5,6]]
-if sample == originalsample:
-    print("Yaay!")
-else:
-    print("Uwongo")
